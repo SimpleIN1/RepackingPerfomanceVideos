8a0d8206d2ccf287a5a1e7ed39e287006ebcc67c
SimpleIN1 <zimforshint_akross@mail.ru>
2025-11-16 02:48:36 +0700

    edited footer style, added encoding image to b64

--- RepackingProject/common/mail/mail_sender.py
+++ RepackingProject/common/mail/mail_sender.py
@@ -1,3 +1,4 @@
+import base64
 import logging
 
 from django.conf import settings
@@ -21,6 +22,11 @@ class SenderEmail:
         self.context["schema"] = settings.SCHEMA
         self.context["domain"] = settings.DOMAIN
         self.context["port"] = settings.PORT
+
+        with open("/static/assets/logo/logo.png", "rb") as f:
+            image_b64 = base64.b64encode(f.read())
+
+        self.context["logo"] = f"data:image/jpg;base64,{image_b64}"
         self.context["support_email"] = settings.SUPPORT_EMAIL
 
         html_message = render_to_string(self.template, context=self.context)
