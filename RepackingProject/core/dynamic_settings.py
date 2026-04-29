7e3eb8b68fdda3c0323265baf7013c8b787016e7
SimpleIN1 <zimforshint_akross@mail.ru>
2026-04-30 03:45:54 +0700

    fixed converting secret params dynamic settings

--- RepackingProject/core/dynamic_settings.py
+++ RepackingProject/core/dynamic_settings.py
@@ -10,13 +10,13 @@ if "migrate" not in sys.argv:
 
     fernet = EncryptedSerializer().get_fernet()
 
-    BBB_SHARED_SECRET = str(fernet.decrypt(global_pref["bbb_settings__bbb_shared_secret"]))
+    BBB_SHARED_SECRET = str(fernet.decrypt(global_pref["bbb_settings__bbb_shared_secret"]).decode())
     BBB_RESOURCE = global_pref["bbb_settings__bbb_resource"]
     BBB_URL = global_pref["bbb_settings__bbb_url"]
 
     NEXTCLOUD_RESOURCE = global_pref["nextcloud_settings__nextcloud_resource"]
     NEXTCLOUD_USER = global_pref["nextcloud_settings__nextcloud_user"]
-    NEXTCLOUD_PASSWORD = str(fernet.decrypt(global_pref["nextcloud_settings__nextcloud_password"]))
+    NEXTCLOUD_PASSWORD = str(fernet.decrypt(global_pref["nextcloud_settings__nextcloud_password"]).decode())
     NEXTCLOUD_PATH = global_pref["nextcloud_settings__nextcloud_path"]
     NEXTCLOUD_SHARE_LINK = global_pref["nextcloud_settings__nextcloud_share_link"]
-    NEXTCLOUD_SHARE_LINK_PASSWORD = str(fernet.decrypt(global_pref["nextcloud_settings__nextcloud_share_link_password"]))
+    NEXTCLOUD_SHARE_LINK_PASSWORD = str(fernet.decrypt(global_pref["nextcloud_settings__nextcloud_share_link_password"]).decode())
