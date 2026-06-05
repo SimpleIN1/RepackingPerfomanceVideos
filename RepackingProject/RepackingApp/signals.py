f67a2b4d8b66f6b9446db02b563b6b77aeb07f5c
SimpleIN1 <zimforshint_akross@mail.ru>
2026-06-05 10:57:19 +0700

    change saving file and added invaling cache signal

--- RepackingProject/RepackingApp/signals.py
+++ RepackingProject/RepackingApp/signals.py
@@ -3,9 +3,14 @@ from django.dispatch import receiver
 from django.db.models.signals import post_delete
 
 from django.conf import settings
-from RepackingApp.models import TypeRecordingModel
+from RepackingApp.models import TypeRecordingModel, RecordingModel
 
 
 @receiver(post_delete, sender=TypeRecordingModel)
 def type_recording_deleted(sender, instance, **kwargs):
     cache.delete(settings.CACHE_TYPE_RECORDINGS)
+
+
+@receiver(post_delete, sender=RecordingModel)
+def type_recording_deleted(sender, instance, **kwargs):
+    cache.delete(settings.CACHE_PK_RECORDINGS.format(instance.type_recording.id))
